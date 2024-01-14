8d8485a6600e6b017e8a3de26c46dd8233b9b652
Yash Wankhade <yash.wankhade@hbwsl.com>
2024-01-15 00:07:28 +0530

    Add UserStory8

---
+++ app/code/Yash/UserStory8/Model/ResourceModel/Employee.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Yash\UserStory8\Model\ResourceModel;
+
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+class Employee extends AbstractDb{
+    protected function _construct(){
+        $this->_init('employee_table', 'employee_id');
+    }
+}
