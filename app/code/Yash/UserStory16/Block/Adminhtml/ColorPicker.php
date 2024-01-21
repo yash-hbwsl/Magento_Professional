f8c1af52c770efc6b5ff1a0c5a0021cbd163770b
Yash Wankhade <yash.wankhade@hbwsl.com>
2024-01-21 19:50:15 +0530

    Add UserStory16

---
+++ app/code/Yash/UserStory16/Block/Adminhtml/ColorPicker.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Yash\UserStory16\Block\Adminhtml;
+
+class ColorPicker extends \Magento\Config\Block\System\Config\Form\Field
+{
+    public function _prepareLayout()
+    {
+        $this->pageConfig->addBodyClass('my-class');
+        return parent::_prepareLayout();
+    }
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $html = $element->getElementHtml();
+        $value = $element->getData('value');
+
+        $html .= '<script type="text/javascript">
+            require(["jquery","jquery/colorpicker/js/colorpicker"], function ($) {
+                $(document).ready(function () {
+                    var $el = $("#' . $element->getHtmlId() . '");
+                    $el.css("backgroundColor", "' . $value . '");
+                    // Attach the color picker
+                    $el.ColorPicker({
+                        color: "' . $value . '",
+                        onChange: function (hsb, hex, rgb) {
+                            $el.css("backgroundColor", "#" + hex).val("#" + hex);
+                        }
+                    });
+                });
+            });
+            </script>';
+        return $html;
+    }
+}
